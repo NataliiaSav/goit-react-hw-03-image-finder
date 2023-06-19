760613aa4dee0a6684429775578a93bddcf418d5
NataliiaSav <savchenko6561@gmail.com>
2023-06-19 13:10:44 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchImages } from '../services/fetch'
 import { ImageGallery } from './ImageGallary/ImageGallery';
+import Modal from './Modal/Modal';
 
 
 export class App extends Component {
@@ -11,6 +12,7 @@ export class App extends Component {
     page: 1,
     isLoading: false,
     isModal: false,
+    currentLargeImg: null,
 
   }
 
@@ -19,7 +21,7 @@ export class App extends Component {
 if(page !== prevState.page || searchImage!== prevState.searchImage ){
   this.getImages(searchImage, page)
     }
-  }
+  };
   getImages = (text, page) => {
     this.setState({ isLoading: true })
     fetchImages(text, page).then(({ data }) => {
@@ -28,12 +30,17 @@ if(page !== prevState.page || searchImage!== prevState.searchImage ){
       }))
     }).catch(error => {
       throw new Error(error)
-    }).finally(()=> {
-      this.setState ({ isLoading: false})
+    }).finally(() => {
+      this.setState({ isLoading: false })
     })
-}
-  openModal = () => {
-    
+  };
+  openModal = (src, alt) => {
+    this.setState(state => ({
+      ...state, currentLargeImg: { src, alt }
+    }))
+  };
+  closeModal = (e) => {
+    this.setState({currentLargeImg: null})
   }
   onSubmit = searchImage => {
     this.setState({ searchImage });
@@ -42,7 +49,9 @@ if(page !== prevState.page || searchImage!== prevState.searchImage ){
     return (
       <>
         <Searchbar onSubmit={this.onSubmit} />
-        {/* <ImageGallery images={this.images} openModal = {this.openModal} /> */}
+        <ImageGallery images={this.state.images} openModal={this.openModal} />
+        
+        {<Modal imgLarge={this.state.currentLargeImg} closeModal={this.closeModal } />}
       </>
     )
   }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css'
 
 export const ImageGalleryItem = ({ itemData, openModal }) => {
     const { webformatURL, largeImageURL, tags } = itemData;
     return (
         <li className ={css.galleryItem}>
-            <img className ={css.imageGalleryItem} src={ webformatURL}alt={tags} onClick ={() => openModal(largeImageURL)} />
+            <img className ={css.imageGalleryItem} src={ webformatURL} alt={tags} onClick ={() => openModal(largeImageURL)} />
         </li>
     )
 }
+ImageGalleryItem.propTypes = {
+    itemData: PropTypes.exact({
+        webformatURL: PropTypes.string,
+        tags: PropTypes.string,
+        largeImageURL: PropTypes.string,
+    }),
+    onClick: PropTypes.func.isRequired,
+};
